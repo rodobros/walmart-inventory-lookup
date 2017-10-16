8c4411160fc63c43cc0ccb43f07534194d78bfc5
rodobros <rodobros@hotmail.com>
2017-10-16 00:04:03 -0400

    added some functionnalities, can now repeat query every few minutes, will play alarm if stock is found

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Little python command-line tool to check a given product stock in some walmart s
 To use, make sure you have python installed.
 
 ## Usage
-walmart_stock_lookup.py [-h] --upc UPC [--sku SKU] [--item_id ITEM_ID] --stores STORES [STORES ...]
+walmart_stock_lookup.py [-h] --upc UPC [--sku SKU] [--item_id ITEM_ID] --stores STORES [STORES ...] [--repeat MINUTE]
 
 arguments:
 * -h, --help :            show this help message and exit
@@ -14,6 +14,7 @@ arguments:
 * --sku SKU :             sku number of item
 * --item_id ITEM_ID :     ID number of item
 * --stores STORES [STORES ...] : Space separated store ids from which to check stock. Ex: --stores 3007 3008. Also accepts files, each stores ids in the file should be on a separate line.
+* --repeat MINUTE : repeat the query every MINUTE minute
 
 ## Examples
 * SNES mini classic inventory lookup
--- walmart_inventory_lookup.py
+++ walmart_inventory_lookup.py
@@ -1,43 +1,70 @@
 import requests
 import json
 import argparse
+import threading
+import time
+import datetime
 
-def print_stock(json_response, store_id, item_id):
-	# formating...
-	hyphensfront = ""
-	hyphenback = ""
-	if len(store_id) % 2 is 0:
-		hyphensfront = "--------------------"[(len(store_id))/2:]
-		hyphensback = hyphensfront
-	else:
-		hyphensfront = "--------------------"[(len(store_id)+1)/2:]
-		hyphensback = hyphensfront + "-"
-	print hyphensfront + " " + store_id + " " + hyphensback
+MAX_QUERY_FREQUENCY = 5 #one query every 5 minute maximum. We don't want to start DDOSing Walmart...
 
-	# print stock information
+def beep():
 	try:
-		print "stock status: " + str(json_response[item_id]["stores"][0]["status"])
-		print "unit available: " + str(json_response[item_id]["stores"][0]["inventory"])
+		lib_playsound = __import__("playsound")
+		lib_playsound.playsound('./alarm.mp3')
+	except:
+		print "ERROR: please download playsound python package for alarm"
+
+def alert_stock(json_response, item_id):
+	should_alert = False
+	print "Query date: " + datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+	try:
+		for store_info in json_response[item_id]["stores"]:
+			if store_info["inventory"] > 0:
+				print "  Store #" + str(store_info["storeNumber"]) + " seems to have stock:"
+				print "    stock status: " + str(store_info["status"])
+				print "    unit(s) available: " + str(store_info["inventory"])
+				should_alert = True
 	except IndexError:
 		print "request timeout or invalid response from server"
+	if should_alert:
+		beep()
+	else:
+		print "  no stock in the Walmarts selected :("
 
-URL = 'https://www.walmart.ca/ws/en/products/availability'
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-client = requests.session()
-# Retrieve the CSRF token first
-client.get(URL)  # sets cookie
-csrftoken = client.cookies['walmart.csrf']
+def run_inventory_lookup(stores_id, item_id, sku, upc):
+	URL = 'https://www.walmart.ca/ws/en/products/availability'
+	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+	client = requests.session()
+	client.get(URL)
+	csrftoken = client.cookies['walmart.csrf']
+
+	payload = {'stores' : '["' + '", "'.join(stores_id) + '"]', 'products' : '{"'+ item_id +'":[{"sku":"'+ sku +'","upc":["' + upc + '"]}]}' , 'csrfToken' : csrftoken, 'origin' : 'pip'}
+	r = client.post(URL, params = payload, headers = headers)
+	alert_stock(json.loads(r.text), item_id)
+
+def run_inventory_lookup_multiple_time(stores_id, item_id, sku, upc, frequency):
+	if frequency < MAX_QUERY_FREQUENCY:
+		frequency = MAX_QUERY_FREQUENCY
+	run_inventory_lookup(stores_id, item_id, sku, upc)
+	t = threading.Timer(frequency*60, run_inventory_lookup_multiple_time, [stores_id, item_id, sku, upc, frequency])
+	t.daemon=True
+	t.start()
+
 
 parser = argparse.ArgumentParser(description='Checks Walmart stocks for a given product in several stores', fromfile_prefix_chars='@')
 parser.add_argument('--upc', help='UPC number of item', required=True)
 parser.add_argument('--sku', help='sku number of item', default='')
 parser.add_argument('--item_id', help='ID number of item', default='')
 parser.add_argument('--stores', help='space separated store ids from which to check stock. Ex: --stores 3007 3008', nargs='+', required=True)
+parser.add_argument('--repeat', type=int, help='repeats the query every X minute. Ex: --repeat 15', default=0)
 
 args = parser.parse_args()
 
-for store_id in args.stores:
-	payload = {'stores' : '["' + store_id + '"]', 'products' : '{"'+ args.item_id +'":[{"sku":"'+ args.sku +'","upc":["' + args.upc + '"]}]}' , 'csrfToken' : csrftoken, 'origin' : 'pip'}
-	r = client.post(URL, params = payload, headers = headers)
-	print_stock(json.loads(r.text), store_id, args.item_id)
+if args.repeat == 0:
+	run_inventory_lookup(args.stores, args.item_id, args.sku, args.upc)
+else:
+	run_inventory_lookup_multiple_time(args.stores, args.item_id, args.sku, args.upc, args.repeat)
+	while True:
+		time.sleep(1)
